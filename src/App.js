46003a797cab9878f1dcc5cc4ff179985b084c2c
OmerFarukBATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2022-06-30 10:12:32 +0300

    react localization example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl';
+
+
+const messages = {
+  "tr-TR": {
+    title: "Selam",
+    description: "{ count } yeni mesajınız var"
+  },
+  "en-US": {
+    title: "Hello",
+    description: "You have { count } new messages"
+  }
+}
 
 function App() {
+
+  const isLocale = localStorage.getItem("locale");
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+  const [locale,setLocale] = useState(defaultLocale);
+
+  useEffect(() => {
+    localStorage.setItem("locale",locale);
+  },[locale])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntlProvider
+        locale={locale}
+        messages={messages[locale]}        
+       >
+
+        <FormattedMessage
+          id="title"
+        />
+        <p> <FormattedMessage
+          id="description"
+          values={{ count: 1 }}
+        /> </p>
+        <br/><br/>
+        <button onClick={() => setLocale("tr-TR")}>TR</button>
+        <br/><br/>
+        <button onClick={() => setLocale("en-US")}>EN</button>
+      </IntlProvider>
     </div>
   );
 }
